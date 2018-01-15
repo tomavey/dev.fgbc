2961621a73d8a8655fcb61a28b94ab1a04646730
tomavey <tomavey@fgbc.org>
2018-01-15 17:41:38 -0500

    jquery for scrollTo

--- public_html/assets/js/custom.js
+++ public_html/assets/js/custom.js
@@ -1,16 +1,22 @@
-function $scrollFromTo(id=""){
-  var from = "a[href$='#" + id + "']";
-  var to = "section#" + id;
-  var $from = $(from);
-  var $to = $(to);
-  $from.click(function() {
-    $('html, body').animate({
-      scrollTop: $to.offset().top
-    }, 1000)
-    return false;
-  })}    
+function $scrollToSectionId(){
+  $("a[href^='#']").click(function() {
+    var id = $(this).attr('href');
+    var to = "section" + id ;
+    if ($(to).length) {
+      $scroll(to);
+  }
+    else {
+      window.location.href = "/" + id;
+    }
+})}    
+
+function $scroll(to){
+  $('html, body').animate({
+    scrollTop: $(to).offset().top
+  }, 1000)
+  return false;
+}
 
 $(document).ready(function () {
-    $scrollFromTo('about');
-    $scrollFromTo('news');
+    $scrollToSectionId();
   });
