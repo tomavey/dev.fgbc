be381d92cd95a52a7a77a13836bc8d02b5dd9e86
tomavey <tomavey@fgbc.org>
2019-08-07 14:48:04 -0400

    init focus dev

---
+++ public_html/focusdev/precache-manifest.bc11e6e86dff8dcf98ebf5a53cf8715e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "015ea96d322aeee56e4c",
+    "url": "/focusdev/js/chunk-2d237524.478ae9dc.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/focusdev/robots.txt"
+  },
+  {
+    "revision": "542cce8cd18ef8690f9e",
+    "url": "/focusdev/css/chunk-7c3869ac.f33fb5ca.css"
+  },
+  {
+    "revision": "c3c411ba0af79c92653c",
+    "url": "/focusdev/js/chunk-2d0b1bc7.4bb20bbf.js"
+  },
+  {
+    "revision": "0ebbd31c7d015de13973",
+    "url": "/focusdev/js/chunk-2d21d9f6.0cbb6197.js"
+  },
+  {
+    "revision": "f142976c9d7d25979823",
+    "url": "/focusdev/js/chunk-2d22d746.743dd9ab.js"
+  },
+  {
+    "revision": "dad02d56ac4fb2b5b0b3",
+    "url": "/focusdev/js/chunk-vendors.8b4b276a.js"
+  },
+  {
+    "revision": "b27b5e4219609727361b",
+    "url": "/focusdev/js/chunk-2d0b380b.ba6c6849.js"
+  },
+  {
+    "revision": "542cce8cd18ef8690f9e",
+    "url": "/focusdev/js/chunk-7c3869ac.570fb1ca.js"
+  },
+  {
+    "revision": "4be9496f754ef065e918",
+    "url": "/focusdev/js/app.7939d7ae.js"
+  },
+  {
+    "revision": "261066efff7dbc4eb28847e951685d4e",
+    "url": "/focusdev/index.html"
+  },
+  {
+    "revision": "dad02d56ac4fb2b5b0b3",
+    "url": "/focusdev/css/chunk-vendors.fa973bb2.css"
+  },
+  {
+    "revision": "4be9496f754ef065e918",
+    "url": "/focusdev/css/app.8e154093.css"
+  }
+];
---
+++ public_html/focusdev/service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/focusdev/precache-manifest.bc11e6e86dff8dcf98ebf5a53cf8715e.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "avey"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
